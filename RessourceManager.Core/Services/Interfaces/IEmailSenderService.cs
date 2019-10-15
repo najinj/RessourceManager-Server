5336d97183ac7062e5f41cf1745149959a2dfe6b
najinj <naji.ensat@gmail.com>
2019-10-15 22:46:13 +0200

    Move IEmailSenderService from Repositores folder to service folder

---
+++ RessourceManager.Core/Services/Interfaces/IEmailSenderService.cs
@@ -0,0 +1,11 @@
+﻿
+
+using System.Threading.Tasks;
+
+namespace RessourceManager.Core.Services.Interfaces
+{
+    public interface IEmailSenderService
+    {
+       Task SendEmailAsync(string email, string subject);
+    }
+}
