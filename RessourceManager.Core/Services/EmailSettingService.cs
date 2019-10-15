ca644dc399aa6f70c976fecae476e8f8a8c396e2
najinj <naji.ensat@gmail.com>
2019-10-15 22:47:17 +0200

    Add EmailSetting Service

---
+++ RessourceManager.Core/Services/EmailSettingService.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using RessourceManager.Core.Models.V1;
+using RessourceManager.Core.Repositories.Interfaces;
+using RessourceManager.Core.Services.Interfaces;
+
+namespace RessourceManager.Core.Services
+{
+    public class EmailSettingService : IEmailSettingService
+    {
+        private readonly IEmailSettingRepository _emailSettingRepository;
+
+        public EmailSettingService(IEmailSettingRepository emailSettingRepository)
+        {
+            _emailSettingRepository = emailSettingRepository;
+        }
+
+        public async Task<EmailSettings> Create(EmailSettings emailSettingsIn)
+        {
+            try
+            {
+                await _emailSettingRepository.Add(emailSettingsIn);
+                return emailSettingsIn;
+            }
+            catch(Exception ex)
+            {
+                return null;
+            }
+            
+        }
+
+        public async Task<EmailSettings> Get()
+        {
+            var emailSettingsList = await _emailSettingRepository.GetAll();
+            return emailSettingsList.FirstOrDefault();       
+        }
+
+        public async void Remove()
+        {
+            var emailSettings = await Get();
+            _emailSettingRepository.Remove(emailSettings.Id);
+        }
+
+        public void Update(EmailSettings emailSettingsIn)
+        {
+            _emailSettingRepository.Update(emailSettingsIn);
+        }
+    }
+}
---
+++ RessourceManager.Core/Services/Interfaces/IEmailSettingService.cs
@@ -0,0 +1,16 @@
+﻿using RessourceManager.Core.Models.V1;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RessourceManager.Core.Services.Interfaces
+{
+    public interface IEmailSettingService
+    {
+        Task<EmailSettings> Get();
+        Task<EmailSettings> Create(EmailSettings emailSettingsIn);
+        void Update(EmailSettings emailSettingsIn);
+        void Remove();
+    }
+}
