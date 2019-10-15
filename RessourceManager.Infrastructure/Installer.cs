6d3452320e9dbe7c58242c9285438776de195c73
najinj <naji.ensat@gmail.com>
2019-10-15 22:50:11 +0200

    Add Installer class into Infustructure project for Database RegisterServices

--- RessourceManager.Infrastructure/Installer.cs
+++ RessourceManager.Infrastructure/Installer.cs
@@ -1,10 +1,22 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
+using RessourceManager.Infrastructure.DatabaseSettings;
+
 
 namespace RessourceManager.Infrastructure
 {
-    class Installer
+    public static class Installer
     {
+        public static void RegisterServices(IServiceCollection services,IConfiguration configuration)
+        {
+            services.Configure<RessourceDatabaseSettings>(x=>
+                      configuration.GetSection(nameof(RessourceDatabaseSettings)).Bind(x));
+            
+            services.AddSingleton<IRessourceDatabaseSettings>(sp =>
+                sp.GetRequiredService<IOptions<RessourceDatabaseSettings>>().Value);
+
+     
+        }
     }
 }
