d45ac79d4a2daa192d787cb0a2eab9d8d4dfc3f8
najinj <naji.ensat@gmail.com>
2019-10-15 22:52:43 +0200

    Update Installer class in Api project  - Register ISpaceRepository  - Register ISpaceService  - Register IAssetRepository  - Register IAssetService  - Register IEmailSettingRepository  - Register IEmailSettingService  - Register IEmailSenderService

--- RessourceManagerApi/Infrastructure/Installer.cs
+++ RessourceManagerApi/Infrastructure/Installer.cs
@@ -16,8 +16,21 @@ namespace RessourceManagerApi.Infrastructure
         public static void ConfigureServices(IServiceCollection services)
         {
             services.AddTransient<IMongoContext, MongoContext>();
+
             services.AddTransient<IRessourceTypeRepository, RessourceTypeRepository>();
             services.AddTransient<IRessourceTypeService, RessourceTypeService>();
+
+            services.AddTransient<ISpaceRepository, SpaceRepository>();
+            services.AddTransient<ISpaceService, SpaceService>();
+
+            services.AddTransient<IAssetRepository, AssetRepository>();
+            services.AddTransient<IAssetService, AssetService>();
+
+            services.AddTransient<IEmailSettingRepository, EmailSettingRepository>();
+            services.AddTransient<IEmailSettingService, EmailSettingService>();
+
+            services.AddTransient<IEmailSenderService, EmailSenderService>();
+
             services.AddTransient<IErrorHandler, ErrorHandler>();
         }
 
