aa5d8de213590855ab98420d3bc111b2073e651b
najinj <naji.ensat@gmail.com>
2019-11-19 23:54:56 +0100

    Add Reservation Service

---
+++ RessourceManager.Core/Services/Interfaces/IReservationService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using RessourceManager.Core.Models.V1;
+
+
+namespace RessourceManager.Core.Services.Interfaces
+{
+    public interface IReservationService
+    {
+        Task<List<Reservation>> Get();
+        Task<Reservation> Get(string reservationIn);
+        Task<Reservation> Create(Reservation reservationIn);
+        Task Update(Reservation reservationIn);
+        Task Remove(string id);
+        Task RemovePeriodicReservations(string periodicId);
+
+    }
+}
---
+++ RessourceManager.Core/Services/ReservationService.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using RessourceManager.Core.Models.V1;
+using RessourceManager.Core.Repositories.Interfaces;
+using RessourceManager.Core.Services.Interfaces;
+
+namespace RessourceManager.Core.Services
+{
+    public class ReservationService : IReservationService
+    {
+        private readonly IReservationRepository _reservationRepository;
+        public ReservationService(IReservationRepository reservationRepository)
+        {
+            _reservationRepository = reservationRepository;
+        }
+
+        public async Task<Reservation> Create(Reservation reservationIn)
+        {
+            var availability = await _reservationRepository.CheckAvailability(reservationIn.Start, reservationIn.End, null, reservationIn.ResourceId);
+            if(availability)
+               await _reservationRepository.Add(reservationIn);
+            return reservationIn;
+        }
+
+        public Task<List<Reservation>> Get()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task<Reservation> Get(string reservationIn)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task Remove(string id)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task RemovePeriodicReservations(string periodicId)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task Update(Reservation reservationIn)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
