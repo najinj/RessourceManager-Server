392cc49325a8f39617ab9d664857acc0c8d710ee
najinj <naji.ensat@gmail.com>
2019-11-19 23:54:41 +0100

    Add Reservation Repository

--- RessourceManager.Core/Repositories/Interfaces/IReservationRepository.cs
+++ RessourceManager.Core/Repositories/Interfaces/IReservationRepository.cs
@@ -1,8 +1,16 @@
 ﻿using RessourceManager.Core.Models.V1;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 
 namespace RessourceManager.Core.Repositories.Interfaces
 {
     public interface IReservationRepository : IRepository<Reservation>
     {
+        Task<List<Reservation>> Get(DateTime start);
+        Task<List<Reservation>> GetReservationsByResource(string resourceId);
+        Task<List<Reservation>> GetReservationsByInterval(DateTime start, DateTime end);
+        Task RemovePeriodicReservations(string periodicId);
+        Task<bool> CheckAvailability(DateTime start, DateTime end, DateTime? periodicEndTime, string spaceId);
     }
 }
--- RessourceManager.Core/Repositories/ReservationRepository.cs
+++ RessourceManager.Core/Repositories/ReservationRepository.cs
@@ -1,14 +1,70 @@
-﻿using RessourceManager.Core.Models.V1;
+﻿using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using RessourceManager.Core.Models.V1;
 using RessourceManager.Core.Repositories.Interfaces;
 using RessourceManager.Infrastructure.Context;
 
 namespace RessourceManager.Core.Repositories
 {
-    public class ReservationRepository : Repository<Reservation> , IReservationRepository
+    public class ReservationRepository : Repository<Reservation>, IReservationRepository
     {
         public ReservationRepository(IMongoContext context) : base(context)
         {
 
+        }
+        public async Task<bool> CheckAvailability(DateTime start, DateTime end, DateTime? periodicEndDate, string spaceId)
+        {
+            if (!periodicEndDate.HasValue)
+            {
+                var reservations = await GetReservationsByInterval(start, end);
+                if(reservations.Exists(reservation=>reservation.ResourceId == spaceId))
+                    return false;
+                return true;
+            }
+            else
+            {
+                for (DateTime startPeriodic = start, endPeriodic = end; startPeriodic <= periodicEndDate; startPeriodic.AddDays(7),endPeriodic.AddDays(7))
+                {
+                    var temp = await GetReservationsByInterval(start, end);
+                    if (temp.Exists(reservation => reservation.ResourceId == spaceId))
+                        return false;
+                }
+                return true;
+            }        
+        }
+
+        public async Task<List<Reservation>> Get(DateTime start)
+        {
+            var reservations = await DbSet.FindAsync(reservation => reservation.Start >= start || reservation.End >= start);
+            return reservations.ToList();
+        }
+
+        public async Task<List<Reservation>> GetReservationsByInterval(DateTime start, DateTime end)
+        {
+            var reservations = await DbSet.FindAsync(reservation => 
+
+                (reservation.Start <= start && reservation.End > start) ||
+
+                (start <= reservation.Start && end >= reservation.End) ||
+
+                (reservation.Start < end && reservation.End >= end) ||
+
+                (reservation.Start > start && reservation.End < end)
+            );
+            return reservations.ToList();
+        }
+
+        public async Task<List<Reservation>> GetReservationsByResource(string resourceId)
+        {
+            var reservations = await DbSet.FindAsync(reservation => reservation.ResourceId == resourceId);
+            return reservations.ToList();
+        }
+
+        public async Task RemovePeriodicReservations(string periodicId)
+        {
+            var result = await DbSet.DeleteManyAsync(reservation => reservation.PeriodicId == periodicId);
         }
     }
 }
