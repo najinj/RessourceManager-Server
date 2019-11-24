7511d36eeeb9076f7b7359bf146460ad95e57049
najinj <naji.ensat@gmail.com>
2019-11-24 22:09:32 +0100

    Add ReservationService Exception class

---
+++ RessourceManager.Core/Exceptions/Reservation/ReservationServiceException.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace RessourceManager.Core.Exceptions.Reservation
+{
+    public class ReservationServiceException : Exception
+    {
+        public string[] Fields
+        {
+            get => Fields;
+            set { Fields = value; }
+        }
+
+        public ReservationServiceException()
+        {
+        }
+
+        public ReservationServiceException(string message, string[] fields)
+            : base(message)
+        {
+            Fields = fields;
+        }
+
+        public ReservationServiceException(string message, Exception inner)
+            : base(message, inner)
+        {
+        }
+    }
+}
