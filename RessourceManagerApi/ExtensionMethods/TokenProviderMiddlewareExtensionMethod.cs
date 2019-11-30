f6de8e7916573ff2c498b9c6b54d9ee897a2d973
najinj <naji.ensat@gmail.com>
2019-11-30 14:19:21 +0100

    Add Token Middleware Extension

---
+++ RessourceManagerApi/ExtensionMethods/TokenProviderMiddlewareExtensionMethod.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Options;
+using RessourceManagerApi.TokenProvider;
+
+namespace RessourceManagerApi.ExtensionMethods
+{
+    public static class MiddlewareExtensions
+    {
+        public static IApplicationBuilder UseTokenProvider(
+            this IApplicationBuilder builder, TokenProviderOptions parameters)
+        {
+            return builder.UseMiddleware<TokenProviderMiddleware>(Options.Create(parameters));
+        }
+    }
+}
