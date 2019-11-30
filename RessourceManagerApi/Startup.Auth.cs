a6e4f5b59cd3cfb7cbbdc5882292cb089ff52dff
najinj <naji.ensat@gmail.com>
2019-11-30 14:22:24 +0100

    Add Startup.Auth partial class

---
+++ RessourceManagerApi/Startup.Auth.cs
@@ -0,0 +1,87 @@
+﻿
+
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.IdentityModel.Tokens;
+using RessourceManagerApi.TokenProvider;
+using System;
+using System.Security.Claims;
+using System.Security.Principal;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RessourceManagerApi
+{
+    public partial class Startup
+    {
+        private  SymmetricSecurityKey _signingKey;
+
+        private  TokenValidationParameters _tokenValidationParameters;
+
+        private  TokenProviderOptions _tokenProviderOptions;
+
+        private void ConfigureAuth(IServiceCollection services)
+        {
+            _signingKey =
+                new SymmetricSecurityKey(
+                    Encoding.ASCII.GetBytes(Configuration.GetSection("TokenAuthentication:SecretKey").Value));
+
+            _tokenValidationParameters = new TokenValidationParameters
+            {
+                // The signing key must match!
+                ValidateIssuerSigningKey = true,
+                IssuerSigningKey = _signingKey,
+                // Validate the JWT Issuer (iss) claim
+                ValidateIssuer = true,
+                ValidIssuer = Configuration.GetSection("TokenAuthentication:Issuer").Value,
+                // Validate the JWT Audience (aud) claim
+                ValidateAudience = true,
+                ValidAudience = Configuration.GetSection("TokenAuthentication:Audience").Value,
+                // Validate the token expiry
+                ValidateLifetime = true,
+                // If you want to allow a certain amount of clock drift, set that here:
+                ClockSkew = TimeSpan.Zero
+            };
+
+
+            _tokenProviderOptions = new TokenProviderOptions
+            {
+                Path = Configuration.GetSection("TokenAuthentication:TokenPath").Value,
+                Audience = Configuration.GetSection("TokenAuthentication:Audience").Value,
+                Issuer = Configuration.GetSection("TokenAuthentication:Issuer").Value,
+                SigningCredentials = new SigningCredentials(_signingKey, SecurityAlgorithms.HmacSha256),
+                IdentityResolver = GetIdentity
+            };
+
+            services.AddAuthentication(options =>
+            {
+                options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            })
+                .AddJwtBearer(options => { options.TokenValidationParameters = _tokenValidationParameters; })
+                .AddCookie(options =>
+                {
+                    options.Cookie.Name = Configuration.GetSection("TokenAuthentication:CookieName").Value;
+                    options.TicketDataFormat = new CustomJwtDataFormat(
+                        SecurityAlgorithms.HmacSha256,
+                        _tokenValidationParameters);
+                });
+
+
+
+
+        }
+
+        private Task<ClaimsIdentity> GetIdentity(string username, string password)
+        {
+            // Don't do this in production, obviously!
+            if (username == "Naji" && password == "Naji")
+            {
+                return Task.FromResult(new ClaimsIdentity(new GenericIdentity(username, "Token"), new Claim[] { }));
+            }
+
+            // Credentials are invalid, or account doesn't exist
+            return Task.FromResult<ClaimsIdentity>(null);
+        }
+    }
+}
