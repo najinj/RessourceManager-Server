0375fb377ad1efbbc85b69b6400844e5803cffe3
nnaji <nnaji@sqli.com>
2020-04-15 18:38:26 +0100

    Remove old Repository Layers for diffrent settings

--- RessourceManager.Core/Repositories/Interfaces/IReservationSettingsRepository.cs
+++
@@ -1,9 +0,0 @@
-﻿using RessourceManager.Core.Models.V1;
-
-namespace RessourceManager.Core.Repositories.Interfaces
-{
-    public interface IReservationSettingsRepository : IRepository<ReservationSettings>
-    {
-
-    }
-}
