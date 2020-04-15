1a478d505c35f67727bcd30f0c80092875ff7757
nnaji <nnaji@sqli.com>
2020-04-15 15:32:54 +0100

    Add Repository Layer for CalendarSettings class

---
+++ RessourceManager.Core/Repositories/CalendarSettingsRepository.cs
@@ -0,0 +1,14 @@
+﻿using RessourceManager.Core.Models.V1;
+using RessourceManager.Core.Repositories.Interfaces;
+using RessourceManager.Infrastructure.Context;
+
+namespace RessourceManager.Core.Repositories
+{
+    public class CalendarSettingsRepository : Repository<CalendarSettings>, ICalendarSettingsRepository
+    {
+        public CalendarSettingsRepository(IMongoContext context) : base(context)
+        {
+
+        }
+    }
+}
---
+++ RessourceManager.Core/Repositories/Interfaces/ICalendarSettingsRepository.cs
@@ -0,0 +1,9 @@
+﻿using RessourceManager.Core.Models.V1;
+
+
+namespace RessourceManager.Core.Repositories.Interfaces
+{
+    public interface ICalendarSettingsRepository : IRepository<CalendarSettings>
+    {
+    }
+}
