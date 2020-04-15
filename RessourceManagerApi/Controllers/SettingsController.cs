b202559109c7c202cd6583ac584cf552e876a1da
nnaji <nnaji@sqli.com>
2020-04-15 18:42:49 +0100

    Add Controller for BackOfficeSettings

---
+++ RessourceManagerApi/Controllers/SettingsController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RessourceManager.Core.Models.V1;
+using RessourceManager.Core.Services.Interfaces;
+using System.Threading.Tasks;
+
+namespace RessourceManagerApi.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class SettingsController : ControllerBase
+    {
+        private readonly IBackOfficeSettingsService _backOfficeSettingsService;
+
+        public SettingsController(IBackOfficeSettingsService backOfficeSettingsService)
+        {
+            _backOfficeSettingsService = backOfficeSettingsService;
+        }
+        // GET: api/Settings
+        [HttpGet]
+        public async Task<BackOfficeSettings> GetSettings() => await _backOfficeSettingsService.Get();
+
+        // POST: api/Settings
+        [HttpPost]
+        public async Task<BackOfficeSettings> AddSettings(BackOfficeSettings settingsIn)
+        {
+            return await _backOfficeSettingsService.Create(settingsIn);
+        }
+
+        // PUT: api/Settings/5
+        [HttpPut("{id}")]
+        public async Task UpdateSettings(string id, BackOfficeSettings settingsIn)
+        {
+            await _backOfficeSettingsService.Update(settingsIn);
+        }
+    }
+}
