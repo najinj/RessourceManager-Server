374fb26c4f7d6ba4a52257934405795445e4672b
nnaji <nnaji@sqli.com>
2020-04-15 18:42:20 +0100

    Add BackOfficeSettings table name to the databaseSettings

--- RessourceManager.Infrastructure/DatabaseSettings/IRessourceDatabaseSettings.cs
+++ RessourceManager.Infrastructure/DatabaseSettings/IRessourceDatabaseSettings.cs
@@ -6,11 +6,10 @@ namespace RessourceManager.Infrastructure.DatabaseSettings
         string AssetsCollectionName { get; set; }
         string SpacesCollectionName { get; set; }
         string RessourceTypesCollectionName { get; set; }
-        string BooksCollectionName { get; set; }
-        string PostsCollectionName { get; set; }
         string UsersCollectionName { get; set; }
         string ReservationsCollectionName { get; set; }
         string EmailSettings { get; set; }
+        string BackOfficeSettings { get; set; }
         string ConnectionString { get; set; }
         string DatabaseName { get; set; }
     }
--- RessourceManager.Infrastructure/DatabaseSettings/RessourceDatabaseSettings.cs
+++ RessourceManager.Infrastructure/DatabaseSettings/RessourceDatabaseSettings.cs
@@ -1,20 +1,14 @@
-﻿
-
-
-
-
-namespace RessourceManager.Infrastructure.DatabaseSettings
+﻿namespace RessourceManager.Infrastructure.DatabaseSettings
 {
     public class RessourceDatabaseSettings : IRessourceDatabaseSettings
     {
         public string AssetsCollectionName { get; set; }
         public string SpacesCollectionName { get; set; }
         public string RessourceTypesCollectionName { get; set; }
-        public string BooksCollectionName { get; set; }
-        public string PostsCollectionName { get; set; }
         public string UsersCollectionName { get; set; }
         public string ReservationsCollectionName { get; set; }
         public string EmailSettings { get; set; }
+        public string BackOfficeSettings { get; set; }
         public string ConnectionString { get; set; }
         public string DatabaseName { get; set; }
     }
