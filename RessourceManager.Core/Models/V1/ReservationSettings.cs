e86d72ffe5b35dac87a9ccba5b141727da249ef7
nnaji <nnaji@sqli.com>
2020-04-15 15:33:50 +0100

    Add ReservationSetting class to the Model to allow More control on booking reservations

---
+++ RessourceManager.Core/Models/V1/ReservationSettings.cs
@@ -0,0 +1,18 @@
+﻿
+
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace RessourceManager.Core.Models.V1
+{
+    public class ReservationSettings
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; }
+        public int MaxNumberOfReservationsPerUser { get; set; }
+        public int MaxDurationPerReservation { get; set; }
+        public int MaxNumberOfReservationsAtSamePeriodPerUser { get; set; }
+        public int IntervalAllowedForReservations { get; set; }
+    }
+}
