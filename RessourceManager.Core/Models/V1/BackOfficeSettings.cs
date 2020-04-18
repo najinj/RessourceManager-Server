b1145f27506af7c9be39544b1a3803446fa3d8be
nnaji <nnaji@sqli.com>
2020-04-19 00:50:54 +0100

    Add FieldTypeAttribute to each property

--- RessourceManager.Core/Models/V1/BackOfficeSettings.cs
+++ RessourceManager.Core/Models/V1/BackOfficeSettings.cs
@@ -1,6 +1,7 @@
 ﻿using MongoDB.Bson;
 using MongoDB.Bson.Serialization.Attributes;
-
+using System;
+using System.ComponentModel;
 
 namespace RessourceManager.Core.Models.V1
 {
@@ -17,27 +18,63 @@ namespace RessourceManager.Core.Models.V1
 
     public class EmailSettings
     {
+        [DisplayName("MailServer")]
+        [FieldTypeAttribute("String")]
         public string MailServer { get; set; }
+        [DisplayName("MailPort")]
+        [FieldTypeAttribute("Integer")]
         public int MailPort { get; set; }
+        [DisplayName("SenderName")]
+        [FieldTypeAttribute("String")]
         public string SenderName { get; set; }
+        [DisplayName("Sender")]
+        [FieldTypeAttribute("String")]
         public string Sender { get; set; }
+        [DisplayName("Password")]
+        [FieldTypeAttribute("String")]
         public string Password { get; set; }
     }
     public class ReservationSettings
     {
+        [FieldTypeAttribute("Integer")]
         public int MaxNumberOfReservationsPerUser { get; set; }
+        [FieldTypeAttribute("Integer")]
         public int MaxDurationPerReservation { get; set; }
+        [FieldTypeAttribute("Integer")]
         public int MaxNumberOfReservationsAtSamePeriodPerUser { get; set; }
+        [FieldTypeAttribute("Integer")]
         public int IntervalAllowedForReservations { get; set; }
     }
 
     public class CalendarSettings
     {
+        [FieldTypeAttribute("Select")]
         public string minTime { get; set; }
+        [FieldTypeAttribute("Select")]
         public string maxTime { get; set; }
-        public string firstDay { get; set; }
+        [FieldTypeAttribute("Select")]
+        public int firstDay { get; set; }
+        [FieldTypeAttribute("Select")]
         public string HourSlotLabelFormat { get; set; }
+        [FieldTypeAttribute("Select")]
         public string MinuteSlotLabelFormat { get; set; }
+        [FieldTypeAttribute("Boolean")]
         public bool Hour12SlotLabelFormat { get; set; }
     }
+
+
+    [AttributeUsage(AttributeTargets.Property)]
+    public class FieldTypeAttributeAttribute : Attribute
+    {
+        private string name;
+        public FieldTypeAttributeAttribute(string name)
+        {
+            this.name = name;
+        }
+        public string Name
+        {
+            get { return name; }
+        }
+    }
+
 }
