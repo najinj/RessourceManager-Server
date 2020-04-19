577bd072abbf7e8cb8641b0035464624ca250fa1
nnaji <nnaji@sqli.com>
2020-04-19 20:20:13 +0000

    Add Validation attributes for start and end dates

--- RessourceManager.Core/ViewModels/Reservation/ReservationViewModel.cs
+++ RessourceManager.Core/ViewModels/Reservation/ReservationViewModel.cs
@@ -3,6 +3,7 @@
 using MongoDB.Bson;
 using MongoDB.Bson.Serialization.Attributes;
 using RessourceManager.Core.Models.V1;
+using RessourceManager.Core.Services.Interfaces;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
@@ -23,9 +24,13 @@ namespace RessourceManager.Core.ViewModels.Reservation
         public string Title { get; set; }
 
         [Required]
+        [DateIntervalValidation]
+        [DateNotInThePastValidation]
         public DateTime Start { get; set; }
 
         [Required]
+        [DateIntervalValidation]
+        [DateNotInThePastValidation(ErrorMessage ="Can't book a reservation in the past")]
         public DateTime End { get; set; }
 
         public string CronoExpression { get; set; }
@@ -39,5 +44,43 @@ namespace RessourceManager.Core.ViewModels.Reservation
 
         public string ResourceName { get; set; }
 
+    }
+
+
+    [AttributeUsage(AttributeTargets.Property)]
+    public class DateIntervalValidationAttribute : ValidationAttribute
+    {
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            var service = (IBackOfficeSettingsService)validationContext
+                                 .GetService(typeof(IBackOfficeSettingsService));
+
+            var settings =  service.Get().Result;
+
+            var reservationSettings = settings.ReservationSettings;
+
+            var inputValue = (DateTime)value;
+
+            var limitDate = DateTime.UtcNow.AddDays(reservationSettings.IntervalAllowedForReservations);
+            var diff = inputValue.Date.CompareTo(limitDate.Date);
+
+            if (diff > 0)
+            {
+                return new ValidationResult($"Can't Add a reservation starting {reservationSettings.IntervalAllowedForReservations} days from today");
+            }
+            return null;
+        }
+    }
+
+    [AttributeUsage(AttributeTargets.Property)]
+    public class DateNotInThePastValidationAttribute : ValidationAttribute
+    {
+        public override bool IsValid(object value)
+        {
+            var inputValue = (DateTime)value;
+            if (DateTime.Today.Date.CompareTo(inputValue.Date) > 0)
+                return false;
+            return true;
+        }
     }
 }
